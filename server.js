0f02333b744b42ef2fe24e5995a3ee994427a192
Scott Brunswig <sbrunswig@gmail.com>
2019-12-13 16:05:23 -0600

    server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(PORT, function(){
+  console.log('app listenting on port');
+});
